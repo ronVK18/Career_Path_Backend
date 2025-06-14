8c846ba8f5946b2d41caa763ee3fb3ff5d113363
Yashparmar2604 <yp842694@gmail.com>
2025-06-14 19:51:50 +0530

    Schema changes

--- Backend/models/admin forms/institutionform.js
+++ Backend/models/admin forms/institutionform.js
@@ -1,24 +1,64 @@
 const mongoose = require("mongoose");
 
 const instituteFormSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  location: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  photo: {
-    type: String, 
-    default: null
-  }
+
+     instituteId:{
+      type:String,
+     },
+     name:{
+      type:String,
+      required:true,
+     },
+     imageUrl:{
+      type:String,
+      default:null
+     },
+     location:{
+      city:{
+        type:String,
+        required:true,
+      },
+      state:{
+        type:String
+      },
+      address:{
+        type:String,
+        required:true
+      }
+     },
+
+     website:{
+      type:String,
+     },
+
+     affilication:{
+      type:String,
+     },
+
+       courses: [
+    {
+      courseId: { 
+        type: String,
+         required: true
+         },
+      fees: { 
+        type: Number,
+         required: true 
+        },
+      seats: { 
+        type: Number, 
+        default: 0
+       },
+      finance_type: {
+         type: String, 
+         default: 'self' }
+    }
+  ],
+
+
+
+  
+
 }, {
   timestamps: true
 });
